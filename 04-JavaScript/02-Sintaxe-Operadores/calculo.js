4e388f2bafa707a938092e22cc5a4e3ee176c611
renatosetubal <remiset@gmail.com>
2022-06-19 10:58:01 -0300

    Ajuste de código na pasta 02-sintaxe-operadores - Redução de código de acordo com boas práticas - Código antigo permaneceu comentado

--- 04-JavaScript/02-Sintaxe-Operadores/calculo.js
+++ 04-JavaScript/02-Sintaxe-Operadores/calculo.js
@@ -4,7 +4,7 @@ n2 = 0;
 function recebeNumeros() {
     let texto = "";
     let resultado = 0;
-    document.getElementById('txt_result').innerHTML="";
+    document.getElementById('txt_result').innerHTML = "";
     /*Recebendo o conteúdo já convertendo para números*/
     let n1 = parseInt(prompt("Digite o primeiro número"));
     let n2 = parseInt(prompt("Ditite o segundo número"));
@@ -16,45 +16,54 @@ function recebeNumeros() {
         return false;
     } else {
         texto = `Os números ${n1} e ${n2} `;
-        eIgual = verificaIgualdade(n1, n2);
-        if (eIgual) {
+       // ehIgual = verificaIgualdade(n1, n2);
+       const ehIgual=n1==n2;
+        if (ehIgual) {
             texto = texto + "são iguais. ";
         } else {
             texto = texto + "não são iguais. ";
         }
         resultado = calcula(n1, n2);
         texto = texto + `A soma dos dois números é ${resultado}, `;
-        if (resultado > 10) {
+        const ehMaiorDez = resultado >= 10;
+        const ehMenorVinte = resultado <= 20;
+        if (ehMaiorDez) {
             texto = texto + `que é maior que 10 e  `;
         } else {
             texto = texto + `que é menor que 10 e  `;
         }
-        if (resultado < 20) {
+        if (ehMenorVinte) {
             texto = texto + `é menor que 20.`;
         } else {
             texto = texto + `é maior que 20.`;
         }
-        document.getElementById('txt_result').innerHTML=texto;
-        // let opcao = parseInt(prompt("Deseja continuar? \n 1 - Continuar \n 2 - Encerrar"));
-        // switch (opcao) {
-        //     case 1: recebeNumeros();
-        //         break;
-        //     case 2: alert("Até mais!"); return false;
-        //     default: return false;
-        // }
     }
+
+
+    // if (resultado < 20) {
+    //     texto = texto + `é menor que 20.`;
+    // } else {
+    //     texto = texto + `é maior que 20.`;
+    // }
+    document.getElementById('txt_result').innerHTML = texto;
+    // let opcao = parseInt(prompt("Deseja continuar? \n 1 - Continuar \n 2 - Encerrar"));
+    // switch (opcao) {
+    //     case 1: recebeNumeros();
+    //         break;
+    //     case 2: alert("Até mais!"); return false;
+    //     default: return false;
+    // }
+// }
 }
 function calcula(n1, n2) {
     return n1 + n2;
 }
-function verificaIgualdade(n1, n2) {
-    if (n1 === n2) {
-        return true;
-    } else {
-        return false;
-    }
-
-
-}
+// function verificaIgualdade(n1, n2) {
+//     if (n1 === n2) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
 
